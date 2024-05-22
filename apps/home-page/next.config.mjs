6097b32622a3ca12c2b566e66680b3eecc5f479d
YC.W <contact@wangyuchen.cn>
2024-05-22 17:58:43 +0800

    feat: publish to github pages

--- apps/home-page/next.config.mjs
+++ apps/home-page/next.config.mjs
@@ -1,6 +1,10 @@
+const isProduction = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: isProduction ? "/auto-git-message" : undefined,
+  assetPrefix: isProduction ? "/auto-git-message" : undefined,
   images: { unoptimized: true },
 };
 
